2ff9da496926c73d687f7769abe6779eebce7401
carla-day <102996623+carla-day@users.noreply.github.com>
2023-02-05 12:44:38 -0500

    completer ch5

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Randmo_Number_File_Writer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numbersToHoldTextBox = new System.Windows.Forms.TextBox();
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numbersToHoldTextBox
+            // 
+            this.numbersToHoldTextBox.Location = new System.Drawing.Point(505, 62);
+            this.numbersToHoldTextBox.Name = "numbersToHoldTextBox";
+            this.numbersToHoldTextBox.Size = new System.Drawing.Size(107, 31);
+            this.numbersToHoldTextBox.TabIndex = 0;
+            this.numbersToHoldTextBox.TextChanged += new System.EventHandler(this.numbersToHoldTextBox_TextChanged);
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.Location = new System.Drawing.Point(129, 62);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(370, 80);
+            this.promptLabel.TabIndex = 1;
+            this.promptLabel.Text = "How many number should the file hold:";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.CreatePrompt = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(123, 245);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(159, 52);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(288, 245);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(159, 52);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 25;
+            this.listBox.Location = new System.Drawing.Point(252, 145);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(221, 79);
+            this.listBox.TabIndex = 4;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(453, 245);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(159, 52);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 410);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.promptLabel);
+            this.Controls.Add(this.numbersToHoldTextBox);
+            this.Name = "Form1";
+            this.Text = "Random Number File Writer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numbersToHoldTextBox;
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Randmo_Number_File_Writer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int numbersToSave;
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //on click close app
+            this.Close();
+        }
+
+        private void numbersToHoldTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //handle empty box
+            if (numbersToHoldTextBox.Text.Length < 1)
+            {
+                MessageBox.Show("Enter a valid integer");
+            }
+            else
+            {
+                //asign the number inputed to the numbers to save
+                numbersToSave = int.Parse(numbersToHoldTextBox.Text);
+
+            }
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+
+        {
+            //initialize streamwriter
+            StreamWriter outputFile;
+            //on click get the value of numbers to save and generate the random numbers
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputFile = File.CreateText(saveFileDialog.FileName);
+                //start for loop to get all  random numbers and wirte to file created
+                //declare int var
+                int num;
+                //create the random object
+                Random rand = new Random();
+                for (int i =0; i < numbersToSave; i++)
+                {
+                   
+                    //assign 
+                    num = rand.Next(1,101);
+
+                    //add num to the file
+                    outputFile.WriteLine(num);
+
+                }
+                outputFile.Close();
+
+                StreamReader inputfile;
+                inputfile = File.OpenText(saveFileDialog.FileName);
+                listBox.Items.Clear();
+                //read file
+                while (!inputfile.EndOfStream)
+                {
+                    // Read an item from the file.
+                    // Process the item. add to the listbox
+
+                    int number = int.Parse( inputfile.ReadLine());
+                    listBox.Items.Add(number);
+
+                }
+
+                //close file
+
+                inputfile.Close(); 
+            }
+            else
+            {
+                MessageBox.Show("The action was cancelled");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //handle by clearing items
+
+            listBox.Items.Clear();  
+            numbersToHoldTextBox.Clear();
+
+        }
+    }
+}
